f15f9a4a99edad631834da37e95df63959760ad7
rbvea <russellbvea@gmail.com>
2014-02-27 20:13:58 -1000

    Not using ng-view.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,5 @@
 'use strict';
 
 angular.module('ngIsotopeApp', [
-  'ngCookies',
-  'ngResource',
-  'ngRoute',
-  'ngSanitize',
   'rbvea.isotope'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5Mode(true);
-  });
+]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
 angular.module('ngIsotopeApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope) {
 
     $scope.images = [
-      {path: "http://placehold.it/300x300", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x400", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "design", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x400", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "design", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "web", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "app", subtitle: "caption"},
-      {path: "http://placehold.it/300x200", category: "design", subtitle: "caption"},
-      {path: "http://placehold.it/300x300", category: "web", subtitle: "caption"},
+      {path: 'http://placehold.it/300x300', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/600x200', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x400', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x200', category: 'design', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x400', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x200', category: 'design', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x200', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x200', category: 'web', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'app', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x200', category: 'design', subtitle: 'caption'},
+      {path: 'http://placehold.it/300x300', category: 'web', subtitle: 'caption'},
     ];
     
   });
